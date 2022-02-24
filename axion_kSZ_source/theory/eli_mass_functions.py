d9773cf0dbb475c0aab652b68ae13194475225af
Eli <emelnick@haverford.edu>
2022-02-24 17:29:09 -0500

    Work on bubble mass functions

--- axion_kSZ_source/theory/eli_mass_functions.py
+++ axion_kSZ_source/theory/eli_mass_functions.py
@@ -37,16 +37,9 @@ class JenkinsMassFunction(MassFunction):
         vals = self.cosmo.rho_mean / m * f * np.fabs(self.sigmaInt.dlogSigma_dlogm(m, z))
         return vals
 
-class EliMassFunction(MassFunction):
-    def __call__(self, m, z):
-        # Output is dN/dlogm
-        vals = np.sqrt(2 / np.pi) * (self.cosmo.rho_mean * self.cosmo.delta_crit / self.sigmaInt(m,z) / m) * np.fabs(self.sigmaInt.dlogSigma_dlogm(m, z)) * np.exp(-self.cosmo.delta_crit**2 / (2 * self.sigmaInt(m, z)**2))
-        return vals
-
 class BubbleMassFunction(MassFunction):
     def __call__(self, m, z, zeta=40):
         # Output is m dn/dm
-
         m_p = 1.672619e-27  #  kg
         G = 6.67408e-11 #  m^3 kg^-1 s^-2
         kb = 1.380649e-23 #  J K^-1
@@ -62,10 +55,13 @@ class BubbleMassFunction(MassFunction):
         # mMin = (3.49203e21) * ((delta * self.cosmo.RHO_C*1000 * Omega_m * (1/self.cosmo.h)**2)**(-1/2)) * ((1+z)**(-3/2)) * (T**(3/2)) # Pre-evaluated (9/(2*np.sqrt(pi))) * ((kb/(G*m_p))**(3/2)) in Mathematica
         # mMin = (3.49203e21) * ((delta * self.cosmo.RHO_C*1000 * self.cosmo.OmegaM)**(-1/2)) * ((1+z)**(-3/2)) * T**(3/2) # Pre-evaluated w/ cosmology.py OmegaM property
         mMin = (2.61739e20) * ((self.cosmo.RHO_C*1000 * self.cosmo.OmegaM)**(-1/2)) * ((1+z)**(-3/2)) * T**(3/2) # Included Delta in pre-eval w/ cosmology.py OmegaM property
-        mMin = 1.91916e33 * (self.cosmo.OmegaM)**(-1/2) * ((1+z)**(-3/2)) * T**(3/2) # Pre-evaluate everything but cosmology.py OmegaM property
+        # mMin = 1.91916e33 * (self.cosmo.OmegaM)**(-1/2) * ((1+z)**(-3/2)) * T**(3/2) # Pre-evaluate everything but cosmology.py OmegaM property
         # mMin = 1.91916e33 * Omega_m**(-1/2) * ((1+z)**(-3/2)) * T**(3/2) # Pre-evaluate everything but Omega_m
 
-
-        B0 = 
-        B = self.cosmo.delta_crit - np.sqrt(2) * erfinv(1 - 1/zeta) * ()
-        vals = np.sqrt(2 / np.pi) * (self.cosmo.rho_mean / m) * np.fabs(self.sigmaInt.dlogSigma_dlogm(m, z)) * (B0 / self.sigmaInt(m, z)) * np.exp(-B**2 / (2 * (self.sigmaInt(m, z))**2))
+        sigma = self.sigmaInt(m,z)
+        sigma_min = self.sigmaInt(mMin,z)
+        K = erfinv(1 - 1/zeta)
+        B0 = self.cosmo.delta_crit - np.sqrt(2) * K * sigma_min
+        B = B0 + K/(np.sqrt(2) * sigma_min)
+        vals = np.sqrt(2 / np.pi) * (self.cosmo.rho_mean / m) * np.fabs(self.sigmaInt.dlogSigma_dlogm(m, z)) * (B0 / sigma * np.exp(-B**2 / (2 * sigma**2))
+        return vals
