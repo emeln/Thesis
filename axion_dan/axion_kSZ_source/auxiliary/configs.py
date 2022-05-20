60c915ee142b2301ed12f862ce9fab278f1e94dd
Eli <emelnick@haverford.edu>
2022-05-20 13:36:42 -0400

    final backups

--- axion_dan/axion_kSZ_source/auxiliary/configs.py
+++
@@ -1,6 +0,0 @@
-param_file_path="/home/emelnick/thesis/axion_dan/axionCAMB-master/inifiles/params.ini "
-axion_camb_path="/home/emelnick/thesis/axion_dan/axionCAMB-master/camb"
-
-cosmo_database="/home/emelnick/thesis/axion_dan/axion_kSZ_source/CAMB_outputs/cosmo_db_final.dat"
-
-camb_outputs_path="/home/emelnick/thesis/axion_dan/axion_kSZ_source/CAMB_outputs/final_out/"
